2ed2bc6a0051807ab59985af942af65c79bbb0d8
DannyM15 <merinodanny15@gmail.com>
2024-03-06 22:24:21 -0800

    fixed Triangle dimesnsinos

--- test/triangle.test.js
+++ test/triangle.test.js
@@ -15,7 +15,7 @@ describe('Triangle', ()=> {
     describe('method', ()=>{
         it('testing the render', ()=>{
             const triangle = new Triangle('asd', 'yellow', 'orange')
-            expect(triangle.render()).toEqual(`<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"><polygon points="150,50 100,150 200,150" fill="orange" stroke-width="5"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="yellow">asd</text> </svg>`)
+            expect(triangle.render()).toEqual(`<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"><polygon points="150, 18 244, 182 56, 182" fill="orange" stroke-width="5"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="yellow">asd</text> </svg>`)
         })
     })
 
